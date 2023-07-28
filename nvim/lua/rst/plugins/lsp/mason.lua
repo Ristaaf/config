d2e9de082f60346f98838448e256d1e3dadbbacb
Rickard Staaf <rickard.staaf@ifs.com>
2023-07-28 14:11:49 +0200

    Fixed arrows and missing json dep

--- nvim/lua/rst/plugins/lsp/mason.lua
+++ nvim/lua/rst/plugins/lsp/mason.lua
@@ -23,6 +23,7 @@ mason_lspconfig.setup({
 		"tsserver",
 		"html",
 		"cssls",
+		"jsonls",
 	},
 })
 
--- nvim/lua/rst/plugins/nvim-tree.lua
+++ nvim/lua/rst/plugins/nvim-tree.lua
@@ -13,8 +13,8 @@ nvimtree.setup({
 		icons = {
 			glyphs = {
 				folder = {
-					arrow_closed = "",
-					arrow_open = "",
+					arrow_closed = "",
+					arrow_open = "",
 				},
 			},
 		},
